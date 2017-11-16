0234f41eb95e5fb1663aa81456895bf411d79a05
zhaopeng-iri <zhaopeng-iri@360.cn>
2017-11-16 14:12:34 +0800

    change rule refreash

--- rule_adapter.go
+++ rule_adapter.go
@@ -20,6 +20,8 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/exec"
+	"path"
 	"path/filepath"
 	"reflect"
 	"strings"
@@ -44,7 +46,6 @@ func main() {
 	redisPassword := updateCmd.Arg("password", "redis path password").Required().String()
 
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
-
 	case updateCmd.FullCommand():
 		os.Exit(RefreshRules(*ruleFilePath, (*redisPath).String(), *redisPassword))
 	}
@@ -54,6 +55,7 @@ func main() {
 type judgeRecored struct {
 	Name string `json:"alarm_name"`
 	Expr string `json:"expre"`
+	Step string `json:"step"`
 }
 
 func RefreshRules(path, redis, password string) int {
@@ -70,9 +72,11 @@ func RefreshRules(path, redis, password string) int {
 
 }
 
-func updateRules(path, redis, password string) int {
+func updateRules(fpath, redis, password string) int {
 	//TODO: filename with job or service name
 	filename := "test"
+	abpath := path.Join(fpath, filename+".yml")
+	absfpath, _ := filepath.Abs(abpath)
 
 	data, err := getRedisData(redis, password)
 	if err != nil {
@@ -87,12 +91,12 @@ func updateRules(path, redis, password string) int {
 	}
 
 	//check rules
-	rulenum, ruleGroups, errsLocal := checkLocalRules(path + "/" + filename + ".yml")
+	rulenum, ruleGroups, errsLocal := checkLocalRules(absfpath)
 
 	//TODO: 文件不存在的时候该如何处理？
 	if errsLocal != nil {
 		fmt.Println("local rules err: ", errsLocal)
-		//return 1
+		return 1
 	}
 
 	localrules := make([]rulefmt.Rule, 0, rulenum)
@@ -100,26 +104,50 @@ func updateRules(path, redis, password string) int {
 		localrules = append(localrules, rg.Rules...)
 	}
 
-	y, remoteRules, err := convertToYaml(remoteRulesMap, filename)
-	errsRemote := checkRulesValid(remoteRules)
+	_, remoteRules, err := convertToYaml(remoteRulesMap, filename)
+	/*
+		errsRemote := checkRulesValid(remoteRules)
 
-	if errsRemote != nil {
-		fmt.Println("remote rules err: ", errsRemote)
+		if errsRemote != nil {
+			fmt.Println("remote rules err: ", errsRemote)
+			return 1
+		}
+	*/
+
+	//ioutil.WriteFile(absfpath)
+
+	updates, nlocalrules := checkUpdate(localrules, remoteRules)
+
+	//update 处理方式应该不一样
+	//localrules = append(localrules, newupdate)
+
+	//nlocalrules = append(localrules, newrules)
+
+	//y, remoteRules, err := convertToYaml(nlocalrules, filename)
+
+	fmt.Println("newlocalrules:", nlocalrules)
+	yamlRG := &rulefmt.RuleGroups{
+		Groups: []rulefmt.RuleGroup{{
+			Name: filename,
+		}},
+	}
+	yamlRG.Groups[0].Rules = nlocalrules
+	y, err := yaml.Marshal(yamlRG)
+
+	if err != nil {
+		fmt.Println("yaml marshal error:", err)
+		return 1
 	}
 
-	ioutil.WriteFile(filename+".yml", y, 0666)
-
-	newrules, newupdate := checkUpdate(localrules, remoteRules)
-
 	isUpdate := false
-	if len(newrules) > 0 || len(newupdate) > 0 {
+	if updates > 0 {
 		isUpdate = true
 	}
 
 	if isUpdate {
 		//TODO: 暂时比较粗暴，rule全刷，如果碰到已存在配置文件的情况可能会把已有内容刷丢。
 		//尽量配合newrules和newupdate列表进行变更。
-		updateRulesFile(y, filename)
+		updateRulesFile(y, absfpath)
 		reloadPromeConfig()
 	}
 
@@ -132,7 +160,7 @@ func getRedisData(path, password string) ([]string, error) {
 		Network:  "tcp",
 		Addr:     path,
 		Password: password,
-		DB:       2,
+		DB:       0,
 	})
 	defer client.Close()
 
@@ -199,7 +227,10 @@ func checkRulesValid(data []rulefmt.Rule) []error {
 
 func checkLocalRules(filename string) (int, []rulefmt.RuleGroup, []error) {
 	fmt.Println("Checking", filename)
-
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		f, _ := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0666)
+		f.Close()
+	}
 	rgs, errs := rulefmt.ParseFile(filename)
 	if errs != nil {
 		return 0, nil, errs
@@ -213,17 +244,28 @@ func checkLocalRules(filename string) (int, []rulefmt.RuleGroup, []error) {
 	return numRules, rgs.Groups, nil
 }
 
-func checkUpdate(localrule, remoterule []rulefmt.Rule) ([]string, []string) {
+func checkUpdate(localrule, remoterule []rulefmt.Rule) (int, []rulefmt.Rule) {
 	var newrules []string
 	var newupdates []string
+	var deletedrules []string
 	var nrule bool
+	var newlocalrule []rulefmt.Rule
+
 	for _, rrule := range remoterule {
+		if err := rrule.Validate(); err != nil {
+			continue
+		}
+		newlocalrule = append(newlocalrule, rrule)
+
+		//check update && new
 		nrule = true
 		for _, lrule := range localrule {
 			if lrule.Record == rrule.Record {
 				nrule = false
 				if lrule.Expr != rrule.Expr || !reflect.DeepEqual(lrule.Labels, rrule.Labels) {
-					newupdates = append(newupdates, lrule.Record)
+					//newupdate 是不是可以去掉
+					newupdates = append(newupdates, rrule.Record)
+					//localrule[n] = rrule
 					break
 				}
 				break
@@ -231,36 +273,66 @@ func checkUpdate(localrule, remoterule []rulefmt.Rule) ([]string, []string) {
 		}
 		if nrule {
 			newrules = append(newrules, rrule.Record)
+			//localrule = append(localrule, rrule)
 		}
 	}
-	return newrules, newupdates
+
+	//check deleted
+	for _, orule := range localrule {
+		deleted := true
+		for _, rule := range newlocalrule {
+			if orule.Record == rule.Record {
+				deleted = false
+			}
+		}
+		if deleted == true {
+			deletedrules = append(deletedrules, orule.Record)
+		}
+	}
+
+	fmt.Println("newrules: ", newrules)
+	fmt.Println("deletedrules: ", deletedrules)
+	fmt.Println("newupdates: ", newupdates)
+
+	updates := len(newrules) + len(newupdates) + len(deletedrules)
+
+	//return newrules, newupdates, newlocalrule
+	return updates, newlocalrule
 }
 
 func updateRulesFile(data []byte, filename string) {
-	ioutil.WriteFile(filename+".yml", data, 0666)
+	fmt.Println(filename)
+	ioutil.WriteFile(filename, data, 0666)
 }
 
 func reloadPromeConfig() {
-	client, err := NewClientForTimeOut()
+	/*
+		client, err := NewClientForTimeOut()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		url := "http://127.0.0.1:9090/-/reload"
+		request, err := http.NewRequest("GET", url, strings.NewReader(""))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		_, err = client.Do(request)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	*/
+	//_, err := exec.Command("sh", "pkill -SIGHUP prometheus").Output()
+	_, err := exec.Command("sh", "-c", "pkill -SIGHUP prometheus").Output()
 	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	url := "http://127.0.0.1:9090/-/reload"
-	request, err := http.NewRequest("GET", url, strings.NewReader(""))
-	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println("prometheus reload Failed", err)
 	}
 
-	_, err = client.Do(request)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 	fmt.Println("prometheus reload Done")
-
 }
 
 func NewClientForTimeOut() (*http.Client, error) {
